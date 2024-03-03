2522735f761233fc253f33ba42192018b314908b
Jay Patel <pjay9981@gmail.com>
2024-03-03 22:57:43 +0530

    Added simple menu page

--- app/admin/class-wpgb-option-page-admin.php
+++ app/admin/class-wpgb-option-page-admin.php
@@ -24,8 +24,39 @@ if ( ! class_exists( 'WPGB_Option_Page_Admin' ) ) {
 		 * Constructor for class.
 		 */
 		public function __construct() {
+			// Register a custom menu page for our settings.
+			add_action( 'admin_menu', array( $this, 'wpgb_op_admin_menu' ) );
 		}
 
+		/**
+		 * Register a custom menu page for our settings.
+		 *
+		 * @return void
+		 */
+		public function wpgb_op_admin_menu() {
+			add_menu_page(
+				esc_html__( 'GB Plugin Settings', 'wpgb-options-page' ),
+				esc_html__( 'GB Plugin Settings', 'wpgb-options-page' ),
+				'manage_options',
+				'wpgb-options-page',
+				array( $this, 'wpgb_op_admin_menu_callback' ),
+				'dashicons-block-default',
+			);
+		}
+
+		/**
+		 * Admin menu callback.
+		 *
+		 * @return void
+		 */
+		public function wpgb_op_admin_menu_callback() {
+			?>
+			<div class="wrap">
+				<h1><?php esc_html_e( 'ACF Geo Field', 'blp-acf-geo-field' ); ?></h1>
+				<div id="wpgb-settings"></div>
+			</div>
+			<?php
+		}
 	}
 	new WPGB_Option_Page_Admin();
 }
